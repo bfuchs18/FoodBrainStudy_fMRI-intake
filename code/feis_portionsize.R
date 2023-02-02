e113c0715369c6fb34adf83c5f8f5bf56be2a34d
bfuchs18 <bari.fuchs@gmail.com>
2023-02-01 19:25:14 -0500

    move merge statements, keep all rows during merge. add note about missing data

--- code/feis_portionsize.R
+++ code/feis_portionsize.R
@@ -39,8 +39,6 @@ kcal_ps_feis <- feis(kcal ~ preFF + avg_vas + meal_order | ps_prop, data = intak
 kcal_ps_feis_data <- data.frame(row.names(slopes(kcal_ps_feis)), slopes(kcal_ps_feis))
 names(kcal_ps_feis_data) <- c('sub', 'kcal_int', 'kcal_ps_lin')
 
-intake_feis_data <- merge(grams_ps_psquad_feis_data, kcal_ps_feis_data, by = 'sub')
-
 ## individual PS slopes for intake - low ED foods ####
 
 # mixed effects models with and without quadratic term
@@ -53,14 +51,10 @@ led_grams_ps_feis <- feis(led_grams ~ preFF + led_vas + meal_order | ps_prop , d
 led_grams_ps_feis_data <- data.frame(row.names(slopes(led_grams_ps_feis)), slopes(led_grams_ps_feis))
 names(led_grams_ps_feis_data) <- c('sub', 'led_grams_int', 'led_grams_ps_lin')
 
-intake_feis_data <- merge(intake_feis_data, led_grams_ps_feis_data, by = 'sub')
-
 # feis model - low ED foods - kcal
 led_kcal_ps_feis <- feis(led_kcal ~ preFF + led_vas + meal_order | ps_prop, data = intake_long, id = "sub")
 led_kcal_ps_feis_data <- data.frame(row.names(slopes(led_kcal_ps_feis)), slopes(led_kcal_ps_feis))
 names(led_kcal_ps_feis_data) <- c('sub', 'led_kcal_int', 'led_kcal_ps_lin')
-
-intake_feis_data <- merge(intake_feis_data, led_kcal_ps_feis_data, by = 'sub')
 
 ## individual PS slopes for intake - high ED foods ####
 
@@ -74,15 +68,21 @@ hed_grams_ps_feis <- feis(hed_grams ~ preFF + hed_vas + meal_order | ps_prop , d
 hed_grams_ps_feis_data <- data.frame(row.names(slopes(hed_grams_ps_feis)), slopes(hed_grams_ps_feis))
 names(hed_grams_ps_feis_data) <- c('sub', 'hed_grams_int', 'hed_grams_ps_lin')
 
-intake_feis_data <- merge(intake_feis_data, hed_grams_ps_feis_data, by = 'sub')
-
 # feis model - high ED foods - kcal
 hed_kcal_ps_feis <- feis(hed_kcal ~ preFF + hed_vas + meal_order | ps_prop, data = intake_long, id = "sub")
 hed_kcal_ps_feis_data <- data.frame(row.names(slopes(hed_kcal_ps_feis)), slopes(hed_kcal_ps_feis))
 names(hed_kcal_ps_feis_data) <- c('sub', 'hed_kcal_int', 'hed_kcal_ps_lin')
 
-intake_feis_data <- merge(intake_feis_data, hed_kcal_ps_feis_data, by = 'sub')
+# merge datasets
+intake_feis_data <- merge(grams_ps_psquad_feis_data, kcal_ps_feis_data, by = 'sub', all = TRUE) #all = T will include all rows in both dataframes*
+intake_feis_data <- merge(intake_feis_data, led_grams_ps_feis_data, by = 'sub', all = TRUE)
+intake_feis_data <- merge(intake_feis_data, led_kcal_ps_feis_data, by = 'sub', all = TRUE)
+intake_feis_data <- merge(intake_feis_data, hed_grams_ps_feis_data, by = 'sub', all = TRUE)
+intake_feis_data <- merge(intake_feis_data, hed_kcal_ps_feis_data, by = 'sub', all = TRUE)
 
 # write database
 write.csv(intake_feis_data,"data/generated/intake_feis.csv", row.names = TRUE)
 
+#### * subs 120 and 128 having missing data for 1 meal each, and therefore do not have enough
+##    datapoints to be included in grams_ps_psquad_feis (which includes quad param). However,
+##    they have enough data to be included in kcal and hed/led models (which do not include quad param)
