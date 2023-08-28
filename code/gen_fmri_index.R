d5bd1c79b5bdb7551c44faa954297fa749e85e4c
bfuchs18 <bari.fuchs@gmail.com>
2023-08-28 15:37:22 -0400

    make seperate index files based on b20 and f20 censoring

--- code/gen_fmri_index.R
+++ code/gen_fmri_index.R
@@ -14,15 +14,23 @@ names(mot_sum)[names(mot_sum) == "id"] <- "sub"
 
 #### Determine number of "good" fmri runs based on motion ####
 
-# define maximum percent of TRs censored in food and non-food blocks (p_censor_interest) for a run to be "good"
+# define maximum percent of TRs censored for a run to be "good"
 censor_thresh = 20
 
-good_run_n <- mot_sum %>%
+# determine n if censoring based on p_censor_blocks (percent of TRs censored in food and office blocks)
+good_run_n_blockcensor <- mot_sum %>%
   group_by(sub) %>%
-  summarize(n_good_runs = sum(p_censor_interest < censor_thresh))
+  summarize(n_good_runs_b20 = sum(p_censor_blocks < censor_thresh))
 
-# pad sub in good_run_n with zeros
-good_run_n$sub <- sprintf("%03d", good_run_n$sub)
+# determine n runs if censoring based on p_censor_food (percent of TRs censored in food blocks)
+good_run_n_foodcensor <- mot_sum %>%
+  group_by(sub) %>%
+  summarize(n_good_runs_f20 = sum(p_censor_food < censor_thresh))
+
+run_count <- merge(good_run_n_blockcensor, good_run_n_foodcensor, by = "sub")
+
+# pad sub in run_count with zeros
+run_count$sub <- sprintf("%03d", run_count$sub)
 
 
 #### Generate inclusion database ####
@@ -35,16 +43,28 @@ meets_inclusion_criteria <- data.frame(sub = fmri_covariates$sub)
 for (i in 1:nrow(meets_inclusion_criteria)) {
   sub_value <- meets_inclusion_criteria$sub[i]
 
-  # check for =>3 good runs
-  if ( sub_value %in% good_run_n$sub ) {
-    if (good_run_n$n_good_runs[good_run_n$sub == sub_value] > 2) {
-      meets_inclusion_criteria$has_3goodruns[meets_inclusion_criteria$sub == sub_value] <- 1
+  # check for =>3 good runs based on n_good_runs_b20
+  if ( sub_value %in% run_count$sub ) {
+    if (run_count$n_good_runs_b20[run_count$sub == sub_value] > 2) {
+      meets_inclusion_criteria$has_3goodruns_b20[meets_inclusion_criteria$sub == sub_value] <- 1
     } else {
-      meets_inclusion_criteria$has_3goodruns[meets_inclusion_criteria$sub == sub_value] <- 0
+      meets_inclusion_criteria$has_3goodruns_b20[meets_inclusion_criteria$sub == sub_value] <- 0
     }
   } else {
-        meets_inclusion_criteria$has_3goodruns[meets_inclusion_criteria$sub == sub_value] <- 0
+        meets_inclusion_criteria$has_3goodruns_b20[meets_inclusion_criteria$sub == sub_value] <- 0
   }
+
+  # check for =>3 good runs based on n_good_runs_f20
+  if ( sub_value %in% run_count$sub ) {
+    if (run_count$n_good_runs_f20[run_count$sub == sub_value] > 2) {
+      meets_inclusion_criteria$has_3goodruns_f20[meets_inclusion_criteria$sub == sub_value] <- 1
+    } else {
+      meets_inclusion_criteria$has_3goodruns_f20[meets_inclusion_criteria$sub == sub_value] <- 0
+    }
+  } else {
+    meets_inclusion_criteria$has_3goodruns_f20[meets_inclusion_criteria$sub == sub_value] <- 0
+  }
+
 
   # check for linear model estimates
   if (fmri_covariates$l_grams_int[fmri_covariates$sub == sub_value] != -999) {
@@ -100,66 +120,124 @@ meets_inclusion_criteria$cortical_fov <- ifelse(meets_inclusion_criteria$sub %in
 # _sr_ = indicates analysis with satiety responsiveness
 # _app = indicates analysis with appetitive mask
 # _cer = indicates analysis with cerebellum mask
+# _b20 = indicates censoring was based on all blocks
+# _b20 = indicates censoring was based on food blocks
 
-# subset subjects for each analysis
-subset_lin_app <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+
+# subset subjects for each analysis -- censoring b20
+subset_lin_app_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                 meets_inclusion_criteria$has_covar == 1 &
                                                 meets_inclusion_criteria$cortical_fov == 1 &
                                                 meets_inclusion_criteria$has_lin_feis == 1, ]
 
-subset_quad_app <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_quad_app_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                   meets_inclusion_criteria$has_covar == 1 &
                                                   meets_inclusion_criteria$cortical_fov == 1 &
                                                   meets_inclusion_criteria$has_quad_feis == 1, ]
 
-subset_fr_app <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_fr_app_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                   meets_inclusion_criteria$has_covar == 1 &
                                                   meets_inclusion_criteria$cortical_fov == 1 &
                                                   meets_inclusion_criteria$has_fr == 1, ]
 
-subset_sr_app <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_sr_app_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                  meets_inclusion_criteria$has_covar == 1 &
                                                  meets_inclusion_criteria$cortical_fov == 1 &
                                                  meets_inclusion_criteria$has_sr == 1, ]
 
 
-subset_lin_cer <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_lin_cer_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                              meets_inclusion_criteria$has_covar == 1 &
                                                              meets_inclusion_criteria$has_lin_feis == 1, ]
 
-subset_quad_cer <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_quad_cer_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                               meets_inclusion_criteria$has_covar == 1 &
                                                               meets_inclusion_criteria$has_quad_feis == 1, ]
 
-subset_fr_cer <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_fr_cer_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                             meets_inclusion_criteria$has_covar == 1 &
                                                             meets_inclusion_criteria$has_fr == 1, ]
 
-subset_sr_cer <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns == 1  &
+subset_sr_cer_b20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_b20 == 1  &
                                                             meets_inclusion_criteria$has_covar == 1 &
                                                             meets_inclusion_criteria$has_sr == 1, ]
 
+# subset subjects for each analysis -- censoring f20
 
+subset_lin_app_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                 meets_inclusion_criteria$has_covar == 1 &
+                                                 meets_inclusion_criteria$cortical_fov == 1 &
+                                                 meets_inclusion_criteria$has_lin_feis == 1, ]
+
+subset_quad_app_f20<- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                  meets_inclusion_criteria$has_covar == 1 &
+                                                  meets_inclusion_criteria$cortical_fov == 1 &
+                                                  meets_inclusion_criteria$has_quad_feis == 1, ]
+
+subset_fr_app_f20<- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                meets_inclusion_criteria$has_covar == 1 &
+                                                meets_inclusion_criteria$cortical_fov == 1 &
+                                                meets_inclusion_criteria$has_fr == 1, ]
+
+subset_sr_app_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                meets_inclusion_criteria$has_covar == 1 &
+                                                meets_inclusion_criteria$cortical_fov == 1 &
+                                                meets_inclusion_criteria$has_sr == 1, ]
+
+
+subset_lin_cer_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                 meets_inclusion_criteria$has_covar == 1 &
+                                                 meets_inclusion_criteria$has_lin_feis == 1, ]
+
+subset_quad_cer_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                  meets_inclusion_criteria$has_covar == 1 &
+                                                  meets_inclusion_criteria$has_quad_feis == 1, ]
+
+subset_fr_cer_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                meets_inclusion_criteria$has_covar == 1 &
+                                                meets_inclusion_criteria$has_fr == 1, ]
+
+subset_sr_cer_f20 <- meets_inclusion_criteria[meets_inclusion_criteria$has_3goodruns_f20 == 1  &
+                                                meets_inclusion_criteria$has_covar == 1 &
+                                                meets_inclusion_criteria$has_sr == 1, ]
 
 # make tab-separated lists
-index_lin_app <- paste(subset_lin_app$sub, collapse="\t")
-index_quad_app <- paste(subset_quad_app$sub, collapse="\t")
-index_fr_app <- paste(subset_fr_app$sub, collapse="\t")
-index_sr_app <- paste(subset_sr_app$sub, collapse="\t")
-index_lin_cer <- paste(subset_lin_cer$sub, collapse="\t")
-index_quad_cer <- paste(subset_quad_cer$sub, collapse="\t")
-index_fr_cer <- paste(subset_fr_cer$sub, collapse="\t")
-index_sr_cer <- paste(subset_sr_cer$sub, collapse="\t")
+index_lin_app_b20 <- paste(subset_lin_app_b20$sub, collapse="\t")
+index_quad_app_b20 <- paste(subset_quad_app_b20$sub, collapse="\t")
+index_fr_app_b20 <- paste(subset_fr_app_b20$sub, collapse="\t")
+index_sr_app_b20 <- paste(subset_sr_app_b20$sub, collapse="\t")
+index_lin_cer_b20 <- paste(subset_lin_cer_b20$sub, collapse="\t")
+index_quad_cer_b20 <- paste(subset_quad_cer_b20$sub, collapse="\t")
+index_fr_cer_b20 <- paste(subset_fr_cer_b20$sub, collapse="\t")
+index_sr_cer_b20 <- paste(subset_sr_cer_b20$sub, collapse="\t")
 
+index_lin_app_f20 <- paste(subset_lin_app_f20$sub, collapse="\t")
+index_quad_app_f20 <- paste(subset_quad_app_f20$sub, collapse="\t")
+index_fr_app_f20 <- paste(subset_fr_app_f20$sub, collapse="\t")
+index_sr_app_f20 <- paste(subset_sr_app_f20$sub, collapse="\t")
+index_lin_cer_f20 <- paste(subset_lin_cer_f20$sub, collapse="\t")
+index_quad_cer_f20 <- paste(subset_quad_cer_f20$sub, collapse="\t")
+index_fr_cer_f20 <- paste(subset_fr_cer_f20$sub, collapse="\t")
+index_sr_cer_f20 <- paste(subset_sr_cer_f20$sub, collapse="\t")
 
 # write tab-separated lists
-writeLines(index_lin_app, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_appetitive.txt")
-writeLines(index_quad_app, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_appetitive.txt")
-writeLines(index_fr_app, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_appetitive.txt")
-writeLines(index_sr_app, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_appetitive.txt")
-writeLines(index_lin_cer, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_cerebellum.txt")
-writeLines(index_quad_cer, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_cerebellum.txt")
-writeLines(index_fr_cer, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_cerebellum.txt")
-writeLines(index_sr_cer, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_cerebellum.txt")
+writeLines(index_lin_app_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_appetitive_b20.txt")
+writeLines(index_quad_app_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_appetitive_b20.txt")
+writeLines(index_fr_app_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_appetitive_b20.txt")
+writeLines(index_sr_app_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_appetitive_b20.txt")
+writeLines(index_lin_cer_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_cerebellum_b20.txt")
+writeLines(index_quad_cer_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_cerebellum_b20.txt")
+writeLines(index_fr_cer_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_cerebellum_b20.txt")
+writeLines(index_sr_cer_b20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_cerebellum_b20.txt")
+
+writeLines(index_lin_app_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_appetitive_f20.txt")
+writeLines(index_quad_app_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_appetitive_f20.txt")
+writeLines(index_fr_app_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_appetitive_f20.txt")
+writeLines(index_sr_app_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_appetitive_f20.txt")
+writeLines(index_lin_cer_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_lin_cerebellum_f20.txt")
+writeLines(index_quad_cer_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_quad_cerebellum_f20.txt")
+writeLines(index_fr_cer_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_fr_cerebellum_f20.txt")
+writeLines(index_sr_cer_f20, "BIDS/derivatives/analyses/foodcue-paper2/R/index_sr_cerebellum_f20.txt")
+
 
 
